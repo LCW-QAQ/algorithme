4b42357dbc61ea3798bf4cc05cd12c6765549985
LCW <lcwliuchongwei@qq.com>
2021-07-26 11:35:17 +0800

    master

---
+++ sort/insertion_sort.py
@@ -0,0 +1,10 @@
+import sort_util
+
+def sort(arr):
+    for i in range(1, len(arr)):
+        for j in range(i, 0, -1):
+            if arr[j] < arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+
+
+sort_util.check(sort)
---
+++ sort/selection_sort.py
@@ -0,0 +1,11 @@
+import sort_util
+
+def sort(arr):
+    for i in range(0, len(arr)):
+        min_idx = i
+        for j in range(i + 1, len(arr)):
+            min_idx = j if arr[min_idx] > arr[j]  else min_idx
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+
+sort_util.check(sort)
