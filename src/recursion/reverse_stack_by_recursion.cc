593a98ffd4f50b447b0e2beaedee3a05ba17ac97
LCW <lcwliuchongwei@qq.com>
2022-02-09 00:13:00 +0800

    k使用递归的方式反转栈

--- src/recursion/reverse_stack_by_recursion.cc
+++ src/recursion/reverse_stack_by_recursion.cc
@@ -3,16 +3,20 @@
 using namespace std;
 
 /**
- * @brief 将栈底元素移动到栈顶
+ * @brief 将栈底元素pop出来
  */
 int pop_to_push_last(stack<int>& stk) {
   auto val = stk.top();
   stk.pop();
   if (stk.empty()) {
+    // 栈空的时候直接返回
     return val;
   } else {
+    // 继续pop
     auto last = pop_to_push_last(stk);
+    // 将上一次的值push
     stk.push(val);
+    // 返回pop出来的值, 因为base case是empty, 所以这个值将会是last(栈底元素)
     return last;
   }
 }
@@ -21,8 +25,10 @@ void reverse_stack(stack<int>& stk) {
   if (stk.empty()) {
     return;
   }
+  // 弹出最后一项
   int last = pop_to_push_last(stk);
   reverse_stack(stk);
+  // 将最后一项push, 巧妙的利用了系统栈
   stk.push(last);
 }
 
