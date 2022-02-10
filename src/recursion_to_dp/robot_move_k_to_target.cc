b2510b9eea2af1dd295239724d79015378c265d8
LCW <lcwliuchongwei@qq.com>
2022-02-10 14:51:04 +0800

    机器人update comment

--- src/recursion_to_dp/robot_move_k_to_target.cc
+++ src/recursion_to_dp/robot_move_k_to_target.cc
@@ -83,6 +83,16 @@ int robot_move_k_to_target_by_dp(int n, int aim, int start, int k) {
   return dp.at(start).at(k);
 }
 
+/*
+题目:
+假设有排成一行的N个位置，记为1~N，N 一定大于或等于 2
+开始时机器人在其中的M位置上(M 一定是 1~N 中的一个)
+如果机器人来到1位置，那么下一步只能往右来到2位置；
+如果机器人来到N位置，那么下一步只能往左来到 N-1 位置；
+如果机器人来到中间位置，那么下一步可以往左走或者往右走；
+规定机器人必须走 K 步，最终能来到P位置(P也是1~N中的一个)的方法有多少种
+给定四个参数 N、M、K、P，返回方法数。
+ */
 int main() {
   cout << robot_move_k_to_target_by_violent_recursion(5, 4, 2, 6) << endl;
   cout << robot_move_k_to_target_by_memory_search(5, 4, 2, 6) << endl;
