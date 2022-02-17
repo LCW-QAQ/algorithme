89852dd61a2f3b10c52dea56050a43f892406a60
LCW <lcwliuchongwei@qq.com>
2022-02-17 15:08:36 +0800

    lru缓存

---
+++ src/cache/lru_cache.hpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+
+template <typename K, typename V>
+struct Node {
+  K key;
+  V value;
+  Node* prev;
+  Node* next;
+  Node() : prev(nullptr), next(nullptr) {}
+  Node(K key, V value) : key(key), value(value), prev(nullptr), next(nullptr) {}
+};
+
+template <typename K, typename V>
+class LruCache {
+ public:
+  std::size_t capacity;
+  Node<K, V>*head, *tail;
+  std::unordered_map<K, Node<K, V>*> cache;
+
+  void add_to_head(Node<K, V>* node) {
+    node->next = head->next;
+    head->next->prev = node;
+    head->next = node;
+    node->prev = head;
+  }
+
+  void remove_node(Node<K, V>* node) {
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+  }
+
+  Node<K, V>* remove_tail() {
+    auto res = tail->prev;
+    remove_node(res);
+    return res;
+  }
+
+  void move_to_head(Node<K, V>* node) {
+    remove_node(node);
+    add_to_head(node);
+  }
+
+  LruCache(std::size_t capacity) : capacity(capacity) {
+    head = new Node<K, V>();
+    tail = new Node<K, V>();
+    head->next = tail;
+  }
+
+  V& get(K&& key) { return get(key); }
+
+  V& get(K& key) {
+    if (!cache.count(key)) {
+      throw "has not key!";
+    }
+    auto node = cache[key];
+    move_to_head(node);
+    return node->value;
+  }
+
+  void insert(K&& key, V&& value) { insert(key, value); }
+
+  void insert(K& key, V& value) {
+    if (!cache.count(key)) {
+      auto new_node = new Node(key, value);
+      cache[key] = new_node;
+      add_to_head(new_node);
+      if (cache.size() > capacity) {
+        auto tail = remove_tail();
+        cache.erase(tail->key);
+        delete tail;
+      }
+    } else {
+      auto node = cache[key];
+      node->value = value;
+      move_to_head(node);
+    }
+  }
+};
+
+/* int main() {
+  using namespace std;
+  LruCache<int, int> cache{4};
+  for (int i = 0; i < 4; i++) {
+    cache.insert(i, i);
+  }
+  cache.insert(99, 99);
+  cache.insert(100, 100);
+  cache.get(2);
+  cache.insert(101, 101);
+  for (auto& [key, node] : cache.cache) {
+    cout << key << ":(" << node->key << ":" << node->value << ")" << endl;
+  }
+  return 0;
+} */
--- src/main.cc
+++ src/main.cc
@@ -1,27 +1,7 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
-// https://leetcode-cn.com/problems/longest-common-subsequence/solution/
-// 动态规划
-class Solution {
- public:
-  int longestCommonSubsequence(string a, string b) {
-    const int alen = a.length(), blen = b.length();
-    if (alen == 0 || blen == 0) {
-      return 0;
-    }
-    vector<vector<int>> dp(alen + 1, vector<int>(blen + 1));
-    for (int i = 1; i <= alen; i++) {
-      for (int j = 1; j <= blen; j++) {
-        // i,j有一个字符相等, 结果就是0..i-1, 0..j-1的值
-        if (a.at(i - 1) == b.at(j - 1)) {
-          dp.at(i).at(j) = dp.at(i - 1).at(j - 1) + 1;
-        } else {  // 看要i或要j那个公共子序列更大
-          dp.at(i).at(j) = max(dp.at(i - 1).at(j), dp.at(i).at(j - 1));
-        }
-      }
-    }
-    return dp.at(alen).at(blen);
-  }
-};
+int main() {
+  std::vector<int> vec;
+  vec.size();
+  std::cout << sizeof(size_t);
+}
